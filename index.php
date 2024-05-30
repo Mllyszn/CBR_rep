2a012f49db875ef7f313f97aea56ac193d3aca9d
Mllyszn <melvinlieuw121107@gmail.com>
2024-05-30 09:55:29 +0200

    dd

--- index.php
+++ index.php
@@ -1,21 +1,36 @@
+<<<<<<< Updated upstream
+=======
+<?php
+include "backend/connect_db.php";
+?>
+
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="assets/css/homepage.css" />
-    <title>CBR Homepage</title>
-  </head>
 
-  <body>
-    <div class="intro">
-      <h1>Welcome to the CBR Test</h1>
-    </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets/css/homepage.css" />
+  <title>CBR Homepage</title>
+</head>
 
-    <div class="btn">
-      <a href="assets/php/question.php">
-        <button>Take the test</button>
-      </a>
-    </div>
-  </body>
+<body>
+  <div class="intro">
+    <h1>Welcome to the CBR Test</h1>
+  </div>
+
+  <div class="btn">
+    <a href="assets/core/question.php">
+      <button>Take the test</button>
+    </a>
+  </div>
+</body>
 </html>
+<<<<<<< Updated upstream
+=======
+
+<?php
+include "backend/core/footer.php";
+?>
+>>>>>>> Stashed changes
